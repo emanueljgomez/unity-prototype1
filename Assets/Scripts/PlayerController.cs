4327060bfeead78aa762874a994657687202fddd
Emanuel Gomez <emanueljgomez@gmail.com>
2022-04-06 13:27:53 -0300

    Tweaks gameplay functionalities related to player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Variables:
-    public float speed = 5.0f;
+    private float speed = 5.0f;
     // The 'f' letter is added at the end to tell the script that this is a float number.
-    // public: access modifier. By giving the variable a 'public' access modifier, it will now appear in the GameObject Inspector.
-    public float turnSpeed;
-    public float horizontalInput;
+    // private / public: access modifiers. By giving the variable a 'public' access modifier, it will appear in the GameObject Inspector.
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
 
     // Start is called before the first frame update
@@ -22,16 +23,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        // This variable receives the Horizontal axis from the Input Manager (Player Controls / Arrow Keys)
+        forwardInput = Input.GetAxis("Vertical");
+        // This variables receive the Horizontal and Vertical axis from the Input Manager (Player Controls / Arrow Keys)
 
-        // MOVE VEHICLE FORWARD (LOGIC):
+        // MOVE VEHICLE FORWARD:
 
         // transform.Translate(0, 0, 1); // <--- This was commented out because there's a cleaner way to implement this.
 
         // transform: calls the 'Transform' component of the GameObject.
         // Translate: translate = translation - This method allows to move the GameObject in the specified axis coordinates.
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); // forwardInput is for Player controlled acceleration.
 
         // Vector3.forward: shorthand for writing Vector3(0, 0, 1)
         // Vector3 refers to a structure that can receive 3D positions and directions, also includes functions for vector operations.
@@ -39,9 +41,8 @@ public class PlayerController : MonoBehaviour
         // Time.deltaTime: deltaTime is the completion time in seconds since the last frame. In this case, it allows movement to become frame-independent.
         // Result: the movement speed will be equal to the 'speed' variable value.
 
-        // TURN VEHICLE RIGHT (LOGIC):
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
-
+        // TURN VEHICLE:
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
 
     }
 }
